b4bc8df37c2fd3e911f9b3a8a94f5560c78d3a7f
bogdanklenov <bogdan@seotlt.ru>
2023-09-08 23:27:16 +0400

    feat: added imports alias, added sort rule for imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'eslint-plugin-import-helpers'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        'import-helpers/order-imports': [
+            'error',
+            {
+                newlinesBetween: 'always',
+                groups: [
+                    'module',
+                    'parent',
+                    'sibling',
+                    'index'
+                ],
+                alphabetize: { order: 'asc', ignoreCase: true },
+            }]
+    },
 }
